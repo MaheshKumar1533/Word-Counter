372730efc9d8708d7fcadc6bb23f8d59f2b3e72b
V Mahesh Kumar <114634598+MaheshKumar1533@users.noreply.github.com>
2024-01-21 10:14:21 +0530

    Update Word Counter.py

--- Word Counter.py
+++ Word Counter.py
@@ -1,10 +1,10 @@
-input_text = input("Enter the Text : ").lower()
-word_list = input_text.split()
+input_text = input("Enter the Text : ").lower() # take input and convert to lower case
+word_list = input_text.split() # split the input into a list of words
 word_count = dict()
 for word in word_list:
     if word not in word_count:
-        word_count[word] = 1
+        word_count[word] = 1 # add the word if it is not present
     else:
-        word_count[word] += 1
+        word_count[word] += 1 # increment count by one if its present
 for word in word_count:
-    print(f"{word} : \t{word_count[word]}")
+    print(f"{word} : \t{word_count[word]}") # print word frequency
